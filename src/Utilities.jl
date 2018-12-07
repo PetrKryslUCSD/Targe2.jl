cae8b744df3ffac9626535cc9049b935429b1853
Petr Krysl <pkrysl@ucsd.edu>
2018-12-06 20:27:34 -0800

    Should I set the executable flag?

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -3,6 +3,7 @@ module Utilities
 using WriteVTK
 
 function _executable()
+    binpath = joinpath(@__DIR__,"..","bin")
     onWindows = Base.Sys.iswindows() 
     if (onWindows)
         exe = "Targe2.exe"
@@ -10,16 +11,20 @@ function _executable()
         onLinux = Base.Sys.islinux() 
         if (onLinux)
             exe = "targe2_GLNXA64"
+            exepath = joinpath(@__DIR__,"..","bin",exe)
+            run(`chmod +x $exepath`)
         else
             onMac = Base.Sys.isapple()
             if (onMac)
                 exe = "targe2_MACI"
+                exepath = joinpath(@__DIR__,"..","bin",exe)
+                run(`chmod +x $exepath`)
             else
                 exe = ""
             end
         end
     end
-    return joinpath(@__DIR__,"..","bin",exe)
+    return joinpath(binpath,exe)
 end
 
 _area2(v, w) = (v[1] * w[2] - v[2] * w[1])
